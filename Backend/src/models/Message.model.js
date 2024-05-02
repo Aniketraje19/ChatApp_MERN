30965e342e263e1bf9557db5a8179a0cc606eeb9
Aniketraje19 <aniketraje865@gmail.com>
2024-05-02 22:16:10 +0530

    Created Message Model

---
+++ Backend/src/models/Message.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const messageSchema = mongoose.Schema({
+    sender:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+    content:{
+        type:String,
+        trim:true,
+    },
+    chat:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Chat"
+    },
+    readBy:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    status:{
+        type:String,
+        enum:["Send","Delivered","Read"],
+        default:"Send"
+    }
+},
+{
+    timestamps:true,
+})
+
+const Message = mongoose.model("Message",messageSchema)
+
+export default Message
